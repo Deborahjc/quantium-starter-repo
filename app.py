741aeb9ff887410a3942e61fccc2373ba48284c6
Deborahjc <allandeborah66@gmail.com>
2022-10-12 15:37:37 +0530

    dash app created

---
+++ app.py
@@ -0,0 +1,26 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv('updated_csv.csv', parse_dates=['date'])
+
+fig = px.line(df, x="date", y="sales", color="region")
+
+app.layout = html.Div(children=[
+    html.H1(
+        children='Pink Morsel Sales'
+    ),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
